c2c49a86ba7ebcf30fd69ad4a95ee8a59490c4ef
22130243VoDaiThanhTai <22130243@st.hcmuaf.edu.vn>
2025-04-02 21:09:03 +0700

    querry tạo table

---
+++ MixiShop/src/main/resources/table.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE mixishop;
+
+USE mixishop;
+
+CREATE TABLE category (
+                          id INT AUTO_INCREMENT PRIMARY KEY,
+                          name VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE product (
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(255) NOT NULL,
+                         image VARCHAR(255),
+                         price INT NOT NULL,
+                         quantity INT NOT NULL,
+                         categoryID INT,
+                         FOREIGN KEY (categoryID) REFERENCES category(id)
+);
+
+CREATE TABLE account (
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         username VARCHAR(255) NOT NULL,
+                         password VARCHAR(255) NOT NULL,
+                         email VARCHAR(255),
+                         address VARCHAR(255),
+                         userID INT,
+                         phone VARCHAR(15)
+);
