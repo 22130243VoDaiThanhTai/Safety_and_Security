f4917eea73e9c303ad438d6b90db1d9217db7e44
22130243VoDaiThanhTai <22130243@st.hcmuaf.edu.vn>
2025-04-16 22:41:02 +0700

    sửa lại table account cho đúng

--- MixiShop/src/main/resources/table.sql
+++ MixiShop/src/main/resources/table.sql
@@ -20,9 +20,9 @@ CREATE TABLE product (
 CREATE TABLE account (
                          id INT AUTO_INCREMENT PRIMARY KEY,
                          username VARCHAR(255) NOT NULL,
-                         password VARCHAR(255) NOT NULL,
+                         password VARCHAR(255),
                          email VARCHAR(255),
                          address VARCHAR(255),
-                         userID INT,
+                         role INT,
                          phone VARCHAR(15)
 );
